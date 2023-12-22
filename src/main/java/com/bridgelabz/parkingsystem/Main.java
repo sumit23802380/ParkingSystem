f44bfb5105b7fed5ea29164fa4164f5b5ba081b1
sumit23802380 <sumitjangid1692153@gmail.com>
2023-12-22 07:09:14 +0530

    Added : Initialized Repository

---
+++ src/main/java/com/bridgelabz/parkingsystem/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.parkingsystem;
+
+/**
+ * @desc : Main Class of Design Parking System
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Parking System");
+    }
+}
